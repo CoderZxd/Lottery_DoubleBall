04de990446e93eb662a289dd7d2f47a9e479df1b
zouxiaodong <zouxiaodong@mobanker.com>
2018-10-27 11:35:35 +0800

    修改为多线程爬取

--- src/main/java/com/zxd/lottery/doubleball/crawler/DoubleBallCrawler.java
+++ src/main/java/com/zxd/lottery/doubleball/crawler/DoubleBallCrawler.java
@@ -6,12 +6,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-
-import static java.util.Collections.sort;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 /**
  * @author CoderZZ
@@ -27,46 +26,66 @@ public class DoubleBallCrawler {
     //03年第1期开奖页面
     private static String url = "http://kaijiang.500.com/shtml/ssq/03001.shtml";
 
-    //TODO:修改多线程
+    //多线程爬取
+    private static int threadNum = 10;
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+
+    //线程安全list,TODO:可以改为普通list
+    private static Vector<ResultDto> resultDtoList = new Vector<ResultDto>(2500);
+
     public static void main(String[] args) throws Exception{
         Document doc = Jsoup.connect(url).get();
         Elements aElements =doc.select(".iSelectList > a");
         if(null != aElements){
             System.out.println("一共期数为:"+aElements.size());
-            List<ResultDto> resultDtoList = new ArrayList<ResultDto>(2500);
-            for(Element a:aElements){
-                try {
-                    ResultDto resultDto = new ResultDto();
-                    resultDto.setNumber(a.text());
-                    resultDto.setHref(a.attr("href"));
-                    doc = Jsoup.connect(a.attr("href")).get();
-                    Elements balls =doc.select("div.ball_box01 li");
-                    for(int i=0;i<balls.size();i++){
-                        Element ball = balls.get(i);
-                        switch (i){
-                            case 0:
-                                resultDto.setRed1(ball.text());
-                            case 1:
-                                resultDto.setRed2(ball.text());
-                            case 2:
-                                resultDto.setRed3(ball.text());
-                            case 3:
-                                resultDto.setRed4(ball.text());
-                            case 4:
-                                resultDto.setRed5(ball.text());
-                            case 5:
-                                resultDto.setRed6(ball.text());
-                            case 6:
-                                resultDto.setBlue(ball.text());
-                        }
-                    }
-                    System.out.println(resultDto.toString());
-                    resultDtoList.add(resultDto);
-                }catch (Exception e){
-                    System.err.println("爬取双色球第"+a.text()+"期开奖结果异常(开奖结果详情页:"+a.attr("href")
-                            + ").异常信息:"+e.getMessage());
-                }
+            for(Element aEle:aElements){
+                ResultDto resultDto = new ResultDto();
+                resultDto.setNumber(aEle.text());
+                resultDto.setHref(aEle.attr("href"));
+                resultDtoList.add(resultDto);
             }
+            ExecutorService executorService = new ScheduledThreadPoolExecutor(threadNum);
+            for(int i=0;i<threadNum;i++){
+                final int localI = i;
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(int j=localI;j<resultDtoList.size();j=j+threadNum) {
+                            ResultDto resultDto = resultDtoList.get(j);
+                            try {
+                                Document detailDoc = Jsoup.connect(resultDto.getHref()).get();
+                                Elements balls = detailDoc.select("div.ball_box01 li");
+                                for(int k=0;k<balls.size();k++){
+                                    Element ball = balls.get(k);
+                                    switch (k){
+                                        case 0:
+                                            resultDto.setRed1(ball.text());
+                                        case 1:
+                                            resultDto.setRed2(ball.text());
+                                        case 2:
+                                            resultDto.setRed3(ball.text());
+                                        case 3:
+                                            resultDto.setRed4(ball.text());
+                                        case 4:
+                                            resultDto.setRed5(ball.text());
+                                        case 5:
+                                            resultDto.setRed6(ball.text());
+                                        case 6:
+                                            resultDto.setBlue(ball.text());
+                                    }
+                                }
+                            } catch (IOException e) {
+                                System.err.println("爬取双色球第"+resultDto.getNumber()+"期开奖结果异常(开奖结果详情页:"+resultDto.getHref()+ ").异常信息:"+e.getMessage());
+                            }
+                        }
+                        System.out.println(Thread.currentThread().getName()+"抓取结束!");
+                        countDownLatch.countDown();
+                    }
+                });
+            }
+            countDownLatch.await();
+            System.out.println("所有线程抓取结束,程序继续执行.........");
             System.out.println(resultDtoList);
             Collections.sort(resultDtoList);
             System.out.println(resultDtoList);
